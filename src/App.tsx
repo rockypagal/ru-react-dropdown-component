eee93a171a50c03fb9b644c87160ef539619caab
Umang Mathod <87117456+rockypagal@users.noreply.github.com>
2025-01-31 21:59:25 +0530

    dropdown added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import DropDownBox from "ru-react-dropdown-component";
+import { options } from "./constant/data";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <DropDownBox options={options} />
     </>
   );
 }
